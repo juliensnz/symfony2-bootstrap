55bfd7c59fa7c5edeaaa7b17092a6e8c2041da93
Julien Sanchez <juliens@nchez.fr>
2013-02-11 10:33:33 +0100

    Ajout du parameters.yml.dist
    Signed-off-by: Julien Sanchez <juliens@nchez.fr>

---
+++ tools/tools/jenkins/beforetest.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp ./app/config/parameters.yml.dist ./app/config/parameters.yml
+
+sed -i "s/DBUSER/jenkins/" ./app/config/parameters.yml
+sed -i "s/DBPASSWORD/jenkins/" ./app/config/parameters.yml
+sed -i "s/DBTESTUSER/jenkins/" ./app/config/parameters.yml
+sed -i "s/DBTESTPASSWORD/jenkins/" ./app/config/parameters.yml
