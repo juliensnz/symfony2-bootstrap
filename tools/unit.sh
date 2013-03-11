f2e29af0b863c40af3f8b4d903c47987013a5b5f
Julien Sanchez <sumbobyboys@gmail.com>
2013-03-11 17:10:09 +0100

    modification de unit.sh

--- tools/unit.sh
+++ tools/unit.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ "$1" == "" ]; then
-    phpunit -c app  --process-isolation
-else
-    phpunit -c app --filter $1  --process-isolation
-fi
+phpunit $@
 
 ./tools/notifier.py "Tests" "DONE"
